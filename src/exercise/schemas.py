2cefd981eb1276f5a391305dcadffe44dd953714
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-25 16:58:20 +0200

    Created EquipmentType, BodyPart and MainUpperMuscles in schemas.py

--- src/exercise/schemas.py
+++ src/exercise/schemas.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Annotated
 from typing import Optional
 
@@ -5,8 +6,33 @@ from annotated_types import MaxLen, MinLen
 from pydantic import BaseModel, HttpUrl
 
 
-class Exercise(BaseModel):
+class EquipmentType(str, Enum):
+    BARBELL = "Barbell"
+    DUMBBELLS = "Dumbbells"
+    DUMBBELL = "Dumbbell"
+    KETTLEBELL = "Kettlebell"
+    OTHER = "Other"
+
+
+class BodyPart(str, Enum):
+    UPPER_BODY = "Upper body"
+    LOWER_BODY = "Lower body"
+    CORE = "Core"
+
+
+class MainUpperMuscles(str, Enum):
+    PECTORAL_MUSCLES = "Pectoral muscles"
+    SHOULDERS = "Shoulders"
+    BICEPS = "Biceps"
+    TRICEPS = "Triceps"
+    FOREARMS = "Forearms"
+    TRAPS = "Traps"
+    LATS = "Lats"
+
+
+class ExerciseCreate(BaseModel):
     name: Annotated[str, MaxLen(100), MinLen(5)]
+    body_part: BodyPart
     target_muscles: Annotated[str, MaxLen(100), MinLen(5)]
     additional_muscle: Optional[Annotated[str, MaxLen(100), MinLen(5)]] = None
     description: Optional[Annotated[str, MaxLen(1000), MinLen(30)]] = (
@@ -17,3 +43,6 @@ class Exercise(BaseModel):
     ] = "No execution description provided"
     image_url: Optional[HttpUrl] = None
     video_url: Optional[HttpUrl] = None
+    is_own_weight: bool = False
+    if not is_own_weight:
+        equipment_type: EquipmentType
