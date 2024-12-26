5e53339d9d20ce4cb479e702512062c9af4a8841
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-26 11:59:01 +0200

    Added DB_ECHO to settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DB_URL: str = "sqlite+aiosqlite:///./test_db.sqlite3"
+    # DB_ECHO: bool = False
+    DB_ECHO: bool = True
 
 
 settings = Settings()
