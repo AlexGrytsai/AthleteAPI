53b36dcad94fd546741ba2a1debbd3fef268ef62
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-26 10:52:18 +0200

    Added docstring to Base class

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -2,4 +2,13 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    """
+    The base class for all database models.
+
+    This class inherits from DeclarativeBase, which is the base class
+    for all SQLAlchemy models.
+    It provides a basic structure for defining database tables
+    and relationships.
+    """
+
     pass
