ad98baf02899352baeb51d98c2a3fbcd96c19730
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-26 11:43:18 +0200

    Added DB_URL to Settings in settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -2,4 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "sqlite+aiosqlite:///./test.db"
+    DB_URL: str = "sqlite+aiosqlite:///./test_db.sqlite3"
+
+
+settings = Settings()
