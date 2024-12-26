2b79d4d157a4e04c99e26819f7e806cec3518da2
Alex Grytsai <a.grytsai1987@gmail.com>
2024-12-26 11:44:04 +0200

    Made get_list_exercises and get_exercise_detail just pass

--- src/exercise/views.py
+++ src/exercise/views.py
@@ -2,14 +2,14 @@ from typing import Annotated, List
 
 from fastapi import APIRouter, Path
 
-from src.exercise.schemas import Exercise
+from src.exercise.schemas import ExerciseCreate
 
 router = APIRouter(prefix="/api/v1/exercises")
 
 
 @router.get("/")
 def get_list_exercises() -> List[Exercise]:
-    return ["Exercise 1", "Exercise 2", "Exercise 3"]
+    pass
 
 
 @router.get("/{exercise_name}/")
@@ -17,5 +17,5 @@ def get_exercise_detail(
     exercise_name: Annotated[
         str, Path(title="The name of the exercise to get", max_length=100)
     ]
-) -> Exercise:
-    return f"Exercise {exercise_name}"
+) -> ExerciseCreate:
+    pass
