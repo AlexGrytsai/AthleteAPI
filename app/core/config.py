03677cdc9141a6efbd8ff5df62fb153f7879eddf
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-09 09:05:21 +0200

    feat: Configure database settings using secret keys
    This commit introduces database configuration using secret keys to enhance security and manageability.
    
    - The settings are fetched using `secret.get_secret_key` with default values if not found.
    - This allows for easy switching of environments without changing the code.
    - Added a `database_url` property to construct the database connection string.

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,21 @@
 from pydantic_settings import BaseSettings
+from app.utils.secret_key import secret
 
 
 class Settings(BaseSettings):
-    pass
+    DB_HOST: str = secret.get_secret_key("DB_HOST", "localhost")
+    DB_PORT: int = secret.get_secret_key("DB_PORT", 5432)
+    DB_USER: str = secret.get_secret_key("DB_USER", "postgres")
+    DB_PASS: str = secret.get_secret_key("DB_PASS", "postgres")
+    DB_NAME: str = secret.get_secret_key("DB_NAME", "athlete_db")
+
+    @property
+    def database_url(self):
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}"
+            f"/{self.DB_NAME}"
+        )
 
 
 settings = Settings()
