a779f7970dfc3ecd4af9d0692a43f3d7151adcb4
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-09 11:34:41 +0200

    feat: Enable environment variables for database configuration
    This change introduces the ability to configure the database connection using environment variables. It loads environment variables from a `.env` file using the `dotenv` library.
    
    The `DB_HOST`, `DB_PORT`, `DB_USER`, `DB_PASS`, and `DB_NAME` settings now default to the values specified in environment variables if they are defined. This allows for easier deployment and configuration management, as the database connection details can be easily changed without modifying the code.

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,20 @@
 import logging.config
+import os
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 from app.utils.secret_key import secret
 
+load_dotenv()
+
 
 class Settings(BaseSettings):
-    DB_HOST: str = secret.get_secret_key("DB_HOST", "localhost")
-    DB_PORT: int = secret.get_secret_key("DB_PORT", 5432)
-    DB_USER: str = secret.get_secret_key("DB_USER", "postgres")
-    DB_PASS: str = secret.get_secret_key("DB_PASS", "postgres")
-    DB_NAME: str = secret.get_secret_key("DB_NAME", "athlete_db")
+    DB_HOST: str = secret.get_secret_key("DB_HOST", os.getenv("DB_HOST"))
+    DB_PORT: int = secret.get_secret_key("DB_PORT", os.getenv("DB_PORT"))
+    DB_USER: str = secret.get_secret_key("DB_USER", os.getenv("DB_USER"))
+    DB_PASS: str = secret.get_secret_key("DB_PASS", os.getenv("DB_PASS"))
+    DB_NAME: str = secret.get_secret_key("DB_NAME", os.getenv("DB_NAME"))
 
     @property
     def database_url(self):
