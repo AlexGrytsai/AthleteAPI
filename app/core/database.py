ab164ac53ec7e17e0b59999c8cffd58da2e6fccc
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-11 18:24:30 +0200

    feat: Implement database connection and error handling
    This commit introduces the database connection logic using SQLAlchemy and asyncpg. It includes:
    
    - Database engine creation with connection pooling.
    - Exception handling for common database connection errors, such as invalid host/port, username/password, and database name.
    - Custom exception raising for better error reporting to the application.

---
+++ app/core/database.py
@@ -0,0 +1,54 @@
+import logging
+import socket
+
+from asyncpg.exceptions import (
+    InvalidAuthorizationSpecificationError,
+    InvalidCatalogNameError,
+)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+
+from app.core.config import settings
+from app.core.exceptions import (
+    InvalidUsernameOrPasswordForDatabase,
+    WrongDatabaseName,
+    DatabaseConnectionErrorWrongHostOrPort,
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def create_database_engine() -> AsyncEngine:
+    try:
+        engine = create_async_engine(
+            url=settings.db_url,
+            echo=False,
+            pool_size=5,
+            max_overflow=10,
+        )
+        async with engine.connect():
+            return engine
+    except socket.gaierror as exc:
+        error_message = (
+            f"Was provided invalid host or port "
+            f"for connection to Database. Trigger exception: "
+            f"{exc.__class__.__name__}.\n"
+            f"Message: {exc}"
+        )
+        raise DatabaseConnectionErrorWrongHostOrPort(error_message)
+    except InvalidAuthorizationSpecificationError as exc:
+        error_message = (
+            f"Was provided invalid username or password "
+            f"for connection to Database. Trigger exception: "
+            f"{exc.__class__.__name__}.\n"
+            f"Message: {exc}"
+        )
+        logger.error(error_message)
+        raise InvalidUsernameOrPasswordForDatabase(error_message)
+    except InvalidCatalogNameError as exc:
+        error_message = (
+            f"Was provided wrong database name. Trigger exception: "
+            f"{exc.__class__.__name__}.\n"
+            f"Message: {exc}"
+        )
+        logger.error(error_message)
+        raise WrongDatabaseName(error_message)
