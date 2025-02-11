d9625b840f5ca4d26095f4b7401bd40a085b9de5
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-11 11:53:28 +0200

    feat: Add decorator to log function execution time
    This commit introduces a new decorator, `sync_timer_of_execution`, which logs the execution time of a function. This helps in identifying performance bottlenecks within the application. The decorator uses `time.perf_counter()` for accurate time measurements and logs the function name and execution time to the logger.

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -17,3 +17,18 @@ def async_timer_of_execution(func: Callable) -> Callable:
         return result
 
     return wrapper
+
+
+def sync_timer_of_execution(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+
+        logger.info(
+            f"Execution time for '{func.__name__}': "
+            f"{end_time - start_time:.4f} seconds"
+        )
+        return result
+
+    return wrapper
