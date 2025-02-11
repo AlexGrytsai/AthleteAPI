ad55fc0757aacda4e569bc725c24e3dc1876c3a1
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-11 12:11:40 +0200

    test: Make database url and parameter retrieval asynchronous
    The database URL and parameter retrieval methods in the `test_config.py` file have been updated to be asynchronous. This change ensures that the tests accurately reflect the asynchronous nature of the code being tested.
    The code now uses `asyncio.run()` to execute the asynchronous methods, and the corresponding tests have been updated to account for this change.

--- app/tests/test_config.py
+++ app/tests/test_config.py
@@ -1,3 +1,4 @@
+import asyncio
 import unittest
 from unittest.mock import patch, MagicMock
 
@@ -26,7 +27,9 @@ class TestDatabaseSettings(unittest.TestCase):
         )
 
         expected_url = "postgresql://test:test@test:test/test"
-        self.assertEqual(self.db_settings.url, expected_url)
+        result = asyncio.run(self.db_settings.url)
+
+        self.assertEqual(result, expected_url)
 
     @patch("app.core.config.SecretKeyBase")
     @patch("app.core.config.DataBaseParameterValidator")
@@ -36,8 +39,8 @@ class TestDatabaseSettings(unittest.TestCase):
             "test_value"
         )
 
-        result = self.db_settings._get_db_param(
-            "USER", default="default_value"
+        result = asyncio.run(
+            self.db_settings._get_db_param("USER", default="default_value")
         )
 
         self.assertEqual(result, "test_value")
@@ -56,7 +59,9 @@ class TestDatabaseSettings(unittest.TestCase):
         self.mock_secret.get_secret_key.return_value = 123
         self.mock_validator.validate_parameter_from_secret.return_value = 123
 
-        result = self.db_settings._get_db_param("PORT", default="5432")
+        result = asyncio.run(
+            self.db_settings._get_db_param("PORT", default="5432")
+        )
         self.assertEqual(result, 123)
         self.mock_secret.get_secret_key.assert_called_with("PORT", "5432")
         self.mock_validator.validate_parameter_from_secret.assert_called_with(
