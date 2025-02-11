0ef83253cf50b305bdd0c0b4b5359b499356a5df
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-11 18:15:26 +0200

    refactor: Simplify ErrorWithGoogleCloudAuthentication
    The ErrorWithGoogleCloudAuthentication class is refactored to take a simple message string in the constructor instead of a GoogleAuthError object. This simplifies the instantiation of the exception and reduces dependencies on Google Cloud libraries. The original implementation included formatting within the exception class, moving the responsiblity of exception message construction to the caller.

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,18 +1,5 @@
-from asyncpg import (
-    InvalidAuthorizationSpecificationError,
-    InvalidCatalogNameError,
-)
-from google.api_core.exceptions import Forbidden
-from google.auth.exceptions import GoogleAuthError
-
-
 class ErrorWithGoogleCloudAuthentication(Exception):
-    def __init__(self, exception: GoogleAuthError):
-        message = (
-            f"Failed to create Google Secret Manager client. "
-            f"Trigger exception: {exception.__class__.__name__}.\n"
-            f"Message: {exception}"
-        )
+    def __init__(self, message: str):
         super().__init__(message)
 
 
