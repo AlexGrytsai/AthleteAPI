bbcb0ced60a9415eca140db819bf53287b1e0a9d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-12 10:02:27 +0200

    refactor: Move database health check to decorator
    This commit refactors the database engine creation process by moving the database health check logic to a dedicated decorator.
    
    - The previous implementation included extensive try-except blocks to handle various connection errors. These checks are moved to `database_health_check` decorator.
    - This change improves code readability and maintainability, separating concerns and simplifying the `create_database_engine` function.
    - The `create_database_engine` function now accepts `database_url` as an argument.

--- app/core/database.py
+++ app/core/database.py
@@ -1,63 +1,17 @@
 import logging
-import socket
 
-from asyncpg.exceptions import (
-    InvalidAuthorizationSpecificationError,
-    InvalidCatalogNameError,
-)
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
-from app.core.config import settings
-from app.core.exceptions import (
-    InvalidUsernameOrPasswordForDatabase,
-    WrongDatabaseName,
-    DatabaseConnectionErrorWrongHostOrPort,
-    ProblemWithConnectionToDatabaseServer,
-)
+from app.utils.decorators import database_health_check
 
 logger = logging.getLogger(__name__)
 
 
-async def create_database_engine() -> AsyncEngine:
-    try:
-        engine = create_async_engine(
-            url=settings.database_url,
-            echo=False,
-            pool_size=5,
-            max_overflow=10,
-        )
-        async with engine.connect():
-            return engine
-    except socket.gaierror as exc:
-        error_message = (
-            f"Was provided invalid host or port "
-            f"for connection to Database. Trigger exception: "
-            f"{exc.__class__.__name__}.\n"
-            f"Message: {exc}"
-        )
-        raise DatabaseConnectionErrorWrongHostOrPort(error_message)
-    except InvalidAuthorizationSpecificationError as exc:
-        error_message = (
-            f"Was provided invalid username or password "
-            f"for connection to Database. Trigger exception: "
-            f"{exc.__class__.__name__}.\n"
-            f"Message: {exc}"
-        )
-        logger.error(error_message)
-        raise InvalidUsernameOrPasswordForDatabase(error_message)
-    except InvalidCatalogNameError as exc:
-        error_message = (
-            f"Was provided wrong database name. Trigger exception: "
-            f"{exc.__class__.__name__}.\n"
-            f"Message: {exc}"
-        )
-        logger.error(error_message)
-        raise WrongDatabaseName(error_message)
-    except ConnectionRefusedError as exc:
-        error_message = (
-            f"Problem with connection to a remote computer. Trigger exception: "
-            f"{exc.__class__.__name__}.\n"
-            f"Message: {exc}"
-        )
-        logger.error(error_message)
-        raise ProblemWithConnectionToDatabaseServer(error_message)
+@database_health_check
+async def create_database_engine(database_url: str) -> AsyncEngine:
+    return create_async_engine(
+        url=database_url,
+        echo=False,
+        pool_size=5,
+        max_overflow=10,
+    )
