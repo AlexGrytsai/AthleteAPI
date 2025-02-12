933024f3c9b00ba07ea8e61bc102fec54aac726d
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-12 14:50:54 +0200

    refactor: Refactor database and Google Cloud exceptions
    This commit refactors the exception classes to improve organization and clarity.
    
    - Introduces base classes `DatabaseError` and `GoogleCloudAuthenticationError` to group related exceptions.
    - Modifies existing database exceptions to inherit from `DatabaseError`.
    - Modifies existing Google Cloud exceptions to inherit from   `GoogleCloudAuthenticationError`.
    - Adds docstrings to the exception classes, explaining the context.
    - Adds `self.message = message` to base exception constructors for better error handling and debugging.

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,28 +1,40 @@
-class ErrorWithGoogleCloudAuthentication(Exception):
+class DatabaseError(Exception):
+    """Basic class for all errors associated with the database."""
+
     def __init__(self, message: str):
         super().__init__(message)
+        self.message = message
 
 
-class DoesNotHavePermissionForGoogleCloudSecret(Exception):
+class InvalidUsernameOrPasswordForDatabase(DatabaseError):
+    """Error: the wrong login or password for connecting to the database."""
+
+
+class WrongDatabaseName(DatabaseError):
+    """Error: The incorrect name of the database is indicated."""
+
+
+class DatabaseConnectionErrorWrongHostOrPort(DatabaseError):
+    """Error: Incorrect host or port for connecting to the database."""
+
+
+class ProblemWithConnectionToDatabaseServer(DatabaseError):
+    """Error: problem with connecting to the database server."""
+
+
+class GoogleCloudAuthenticationError(Exception):
+    """Basic class for authentication errors in Google Cloud."""
+
+
+class ErrorWithGoogleCloudAuthentication(GoogleCloudAuthenticationError):
+    """Authentication error in Google Cloud."""
+
     def __init__(self, message: str):
         super().__init__(message)
+        self.message = message
 
 
-class InvalidUsernameOrPasswordForDatabase(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class WrongDatabaseName(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class DatabaseConnectionErrorWrongHostOrPort(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class ProblemWithConnectionToDatabaseServer(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
+class DoesNotHavePermissionForGoogleCloudSecret(
+    GoogleCloudAuthenticationError
+):
+    """Ошибка: Недостаточно прав для доступа к Google Cloud Secret."""
