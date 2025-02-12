2771ecd60112517e93e291f1561c2ba16e07f9ff
Alex Grytsai <a.grytsai1987@gmail.com>
2025-02-12 09:40:51 +0200

    feat: Add exception handling for connection refused errors
    This commit adds exception handling for `ConnectionRefusedError` when connecting to the database.  This enhances the application's robustness by providing more informative error messages and handling cases where the database server is unreachable.  A new custom exception `ProblemWithConnectionToDatabaseServer` is raised, providing specific details about the connection refusal.

--- app/core/database.py
+++ app/core/database.py
@@ -12,6 +12,7 @@ from app.core.exceptions import (
     InvalidUsernameOrPasswordForDatabase,
     WrongDatabaseName,
     DatabaseConnectionErrorWrongHostOrPort,
+    ProblemWithConnectionToDatabaseServer,
 )
 
 logger = logging.getLogger(__name__)
@@ -52,3 +53,11 @@ async def create_database_engine() -> AsyncEngine:
         )
         logger.error(error_message)
         raise WrongDatabaseName(error_message)
+    except ConnectionRefusedError as exc:
+        error_message = (
+            f"Problem with connection to a remote computer. Trigger exception: "
+            f"{exc.__class__.__name__}.\n"
+            f"Message: {exc}"
+        )
+        logger.error(error_message)
+        raise ProblemWithConnectionToDatabaseServer(error_message)
